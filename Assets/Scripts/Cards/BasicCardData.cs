ef34365969d553f922f919ade8c92c5892918340
Alessandro Cois <cois.alessandro@gmail.com>
2025-08-03 11:38:43 -0700

    Make stats more table looking

--- Assets/Scripts/Cards/BasicCardData.cs
+++ Assets/Scripts/Cards/BasicCardData.cs
@@ -8,18 +8,18 @@ using Random = System.Random;
  */
 public class BasicCardData : CardData
 {
-    public static BasicCardData Goblin = new BasicCardData(CreatureType.Goblin,  2, 2, 0, 0, 2, 1, 0, 0, 0, 0.2f);
-    public static BasicCardData Fairy = new BasicCardData(CreatureType.Fairy, 1, 1, 0, 2, 1, 1, 0, 0,1, 1);
-    public static BasicCardData Witch = new BasicCardData(CreatureType.Witch, 2, 1, 0, 1, 1, 1, 0, 0,1, 1);
-    public static BasicCardData Demon = new BasicCardData(CreatureType.Demon, 1, 2, 1, 1, 1, 1, 0, 0,0, 0.8f);
-    public static BasicCardData Werewolf = new BasicCardData(CreatureType.Werewolf, 2, 1, 2, 0, 1, 1, 0, 0,0, 0.4f);
-    public static BasicCardData Giant = new BasicCardData(CreatureType.Giant, 2, 2, 2, 0, 1, 0, 0, 0,0, 0.2f);
-    public static BasicCardData Vampire = new BasicCardData(CreatureType.Vampire, 1, 1, 1, 1, 1, 1, 0, 0,1, 0.6f);
+    public static BasicCardData Goblin = new BasicCardData(CreatureType.Goblin,           2, 2, 0, 0, 2, 1, 0, 0, 0, 0.2f);
+    public static BasicCardData Fairy = new BasicCardData(CreatureType.Fairy,             1, 1, 0, 2, 1, 1, 0, 0,1, 1);
+    public static BasicCardData Witch = new BasicCardData(CreatureType.Witch,             2, 1, 0, 1, 1, 1, 0, 0,1, 1);
+    public static BasicCardData Demon = new BasicCardData(CreatureType.Demon,             1, 2, 1, 1, 1, 1, 0, 0,0, 0.8f);
+    public static BasicCardData Werewolf = new BasicCardData(CreatureType.Werewolf,       2, 1, 2, 0, 1, 1, 0, 0,0, 0.4f);
+    public static BasicCardData Giant = new BasicCardData(CreatureType.Giant,             2, 2, 2, 0, 1, 0, 0, 0,0, 0.2f);
+    public static BasicCardData Vampire = new BasicCardData(CreatureType.Vampire,         1, 1, 1, 1, 1, 1, 0, 0,1, 0.6f);
     public static BasicCardData Abomination = new BasicCardData(CreatureType.Abomination, 1, 2, 0, 1, 1, 1, 0, 0,1, 0.8f);
-    public static BasicCardData Ghost = new BasicCardData(CreatureType.Ghost, 0, 1, 0, 0, 2, 2, 0, 0,2, 0.6f);
-    public static BasicCardData Ghoul = new BasicCardData(CreatureType.Ghoul, 2, 1, 1, 1, 1, 0, 0, 0,1, 0.4f);
-    public static BasicCardData Zombie = new BasicCardData(CreatureType.Zombie, 1, 1, 0, 1, 2, 2, 0, 0,0, 0);
-    public static BasicCardData Vermin = new BasicCardData(CreatureType.Vermin, 2, 1, 1, 0, 2, 1, 0, 0,0, 0);
+    public static BasicCardData Ghost = new BasicCardData(CreatureType.Ghost,             0, 1, 0, 0, 2, 2, 0, 0,2, 0.6f);
+    public static BasicCardData Ghoul = new BasicCardData(CreatureType.Ghoul,             2, 1, 1, 1, 1, 0, 0, 0,1, 0.4f);
+    public static BasicCardData Zombie = new BasicCardData(CreatureType.Zombie,           1, 1, 0, 1, 2, 2, 0, 0,0, 0);
+    public static BasicCardData Vermin = new BasicCardData(CreatureType.Vermin,           2, 1, 1, 0, 2, 1, 0, 0,0, 0);
     
     public static List<BasicCardData> baseCardsToGenerateFrom = new List<BasicCardData>
     {
